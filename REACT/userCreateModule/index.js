4111d1d953f9a319ae680dbd33627ccc44fb7ba0
{iamabhi0619} <{iamabhi0619@gmail.com}>
2024-03-02 16:05:02 +0530

    update

---
+++ REACT/userCreateModule/index.js
@@ -0,0 +1,6 @@
+import {name, sumDATA as sum} from "./calculator.js";
+//example os alais
+import {subDATA} from "./calculator.js"
+console.log(sum(80,20));
+console.log(subDATA(80,20));
+console.log(name);
---
+++ TailWind CSS/tailwind.config.js
@@ -0,0 +1,10 @@
+tailwind.config = {
+    theme: {
+      extend: {
+        colors: {
+          backgr: '#ADC2FF',
+          backgr2: '#FF1919',
+        }
+      }
+    }
+  }
